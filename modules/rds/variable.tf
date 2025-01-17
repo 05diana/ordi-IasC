4dec102055a377b6baf359a0a7a53c1790303582
05diana <111016421+05diana@users.noreply.github.com>
2025-01-17 09:29:54 -0500

    Create rds/variable.tf

---
+++ modules/rds/variable.tf
@@ -0,0 +1,10 @@
+
+variable "db_name" {}
+variable "db_user" {}
+variable "multi_az" {}
+variable "db_storage" {}
+variable "db_version" {}
+variable "db_password" {}
+variable "postgresql_sg_id" {}
+variable "db_instance_class" {}
+variable "private_subnet_ids" {}
